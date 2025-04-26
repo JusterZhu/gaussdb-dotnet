fbd53a110d7612dd6324dbfbf697468f55d86ab3
Juster Zhu <zhuzhen723723@outlook.com>
2025-04-26 22:44:15 +0800

    feature: Example of adding and querying data. (#4)
    * feature: Example of adding and querying data.

--- example/GetStarted/Program.cs
+++ example/GetStarted/Program.cs
@@ -15,9 +15,33 @@ if (conn.State is ConnectionState.Closed)
 Console.WriteLine($@"Connection state: {conn.State}");
 
 {
+    await TestScalar();
+    await TestReader();
+}
+
+Console.WriteLine(@"Completed!");
+
+
+async Task TestScalar()
+{
     await using var cmd = new NpgsqlCommand("SELECT 1", conn);
     var result = await cmd.ExecuteScalarAsync();
     Console.WriteLine(result);
 }
 
-Console.WriteLine(@"Completed!");
+async Task TestReader()
+{
+    await using (var cmd = new NpgsqlCommand("SELECT * FROM employees", conn))
+    {
+        await using var reader = await cmd.ExecuteReaderAsync();
+        while (await reader.ReadAsync())
+        {
+            // 读取每一行数据
+            var id = reader.GetInt32(0); // 假设第一列是ID，类型为int
+            var name = reader.GetString(1); // 假设第二列是Name，类型为varchar
+            var age = reader.GetInt32(2); // 假设第三列是Age，可为空
+
+            Console.WriteLine($"ID: {id}, Name: {name}, Age: {age}");
+        }
+    }
+}
